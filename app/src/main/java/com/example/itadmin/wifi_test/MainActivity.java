de78f82f48059ff41c08c3596a68452e0c52cf69
ccasbre27@hotmail.com <ccasbre27@hotmail.com>
2016-08-15 11:25:13 -0600

    Se obtiene el porcentaje de la señal y se agregan intervalos para los decibeles de manera que se puede saber si es una señal excelente, buena, razonable o débil

--- app/src/main/java/com/example/itadmin/wifi_test/MainActivity.java
+++ app/src/main/java/com/example/itadmin/wifi_test/MainActivity.java
@@ -13,7 +13,8 @@ import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     WifiManager mWifiManager;
     WifiReceiver mWifiReceiver;
@@ -28,61 +29,107 @@ public class MainActivity extends AppCompatActivity {
 
     http://stackoverflow.com/questions/18831442/how-to-get-signal-strength-of-connected-wifi-android
 
+    Mobile data
+    http://stackoverflow.com/questions/18399364/get-signal-strength-of-wifi-and-mobile-data
+
+    http://mobilemerit.com/android-app-for-detecting-wifi-signal-with-source-code/
 
      */
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         txtvSignal = (TextView) findViewById(R.id.txtvSignal);
 
         mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        if (!mWifiManager.isWifiEnabled()) {
-            // If wifi disabled then enable it
+
+        // se verifica si el wifi está habilitado
+        if (!mWifiManager.isWifiEnabled())
+        {
             Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled",
                     Toast.LENGTH_LONG).show();
             mWifiManager.setWifiEnabled(true);
         }
+
         mWifiReceiver = new WifiReceiver();
+
+        // se registra el receiver
         IntentFilter mIntentFilter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
         mIntentFilter.addAction(WifiManager.RSSI_CHANGED_ACTION);
         registerReceiver(mWifiReceiver, mIntentFilter);
+
         mWifiManager.startScan();
     }
 
-    public void onPause() {
+    public void onPause()
+    {
         unregisterReceiver(mWifiReceiver);
         super.onPause();
     }
 
-    public void onResume() {
+    public void onResume()
+    {
         registerReceiver(mWifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         super.onResume();
     }
 
-    class WifiReceiver extends BroadcastReceiver {
-        // This method call when number of wifi connections changed
-        public void onReceive(Context c, Intent intent) {
+    class WifiReceiver extends BroadcastReceiver
+    {
+
+        // Es disparado cada vez que la señal del wifi cambia
+        public void onReceive(Context c, Intent intent)
+        {
             int state = mWifiManager.getWifiState();
+
+            // cantidad de rayas
             int maxLevel = 5;
-            if (state == WifiManager.WIFI_STATE_ENABLED) {
-                // Get Scanned results in an array List
+
+            if (state == WifiManager.WIFI_STATE_ENABLED)
+            {
+                // Se obtiene la lista de accesos a wifi disponibles
                 wifiList = mWifiManager.getScanResults();
-                // Iterate on the list
-                for (ScanResult result : wifiList) {
-                    //The level of each wifiNetwork from 0-5
+
+                // recorremos cada acceso
+                for (ScanResult result : wifiList)
+                {
+                    // La señal ronda entre 0-5
                     int level = WifiManager.calculateSignalLevel(
                             result.level,maxLevel);
-                    String SSID = result.SSID;
-                    String capabilities = result.capabilities;
-                    // TODO add your own code.
 
-                    txtvSignal.setText(txtvSignal.getText().toString() + level);
+                    // para obtener el porcentaje
+                    int rssi = mWifiManager.getConnectionInfo().getRssi();
+                    int level2 = WifiManager.calculateSignalLevel(rssi, 10);
+                    int percentage = (int) ((level2/10.0)*100);
+
+                    // Excellent >-50 dBm
+                    if (result.level > - 50)
+                    {
+                        txtvSignal.setText(txtvSignal.getText().toString() + "\nExcellent " + level + " = " + percentage);
+                    }
+                    // Good -50 to -60 dBm
+                    else if (result.level > -60)
+                    {
+                        txtvSignal.setText(txtvSignal.getText().toString() + "\nGood " + level + " = " + percentage);
+                    }
+                    // Fair -60 to -70 dBm
+                    else if (result.level > -70)
+                    {
+                        txtvSignal.setText(txtvSignal.getText().toString() + "\nFair " + level + " = " + percentage);
+                    }
+                    // Weak < -70 dBm
+                    else if (result.level < -70)
+                    {
+                        txtvSignal.setText(txtvSignal.getText().toString() + "\nWeak " + level + " = " + percentage);
+                    }
+                    
                 }
             }
         }
     }
 
 }
+
+
